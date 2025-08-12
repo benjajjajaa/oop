b04beebaec2356aaa70be9982894f5f5e3c61be3
benjajjajaa <idk.bnjstin@gmail.com>
2025-08-12 17:44:06 +0800

    if statement(1)

--- labact3/Program.cs
+++ labact3/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace labact3
+namespace labact3 //BEN JUSTIN Z. ESPIRITU if-statement(1)
 {
     internal class Program
     {
