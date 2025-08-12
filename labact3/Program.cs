1a8ffb611fded44e4d84bd9cb9b4a63da5ec781e
benjajjajaa <idk.bnjstin@gmail.com>
2025-08-12 17:40:08 +0800

    if statement

--- labact3/Program.cs
+++ labact3/Program.cs
@@ -10,6 +10,38 @@ namespace labact3
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter your grade: ");
+            int score = Convert.ToInt32(Console.ReadLine());
+
+
+            if (score >= 90)
+            {
+                Console.WriteLine("-----------------------------");
+                Console.WriteLine("Your Grade is: A");
+            }
+
+
+            if (score < 90 && score >= 80)
+            {
+                Console.WriteLine("-----------------------------");
+                Console.WriteLine("Your Grade is: B");
+            }
+
+
+            if (score < 80 && score >= 70)
+            {
+                Console.WriteLine("-----------------------------");
+                Console.WriteLine("Your Grade is: C");
+            }
+
+
+            if (score < 70)
+            {
+                Console.WriteLine("-----------------------------");
+                Console.WriteLine("Your Grade is: F");
+            }
+            Console.ReadKey();
         }
     }
 }
+ 
